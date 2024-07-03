22e636a68694cbab15204115c80c86c725116308
NomedXD <coovshow1@gmail.com>
2024-07-03 16:04:41 +0300

    feat(BankApiController): Add Swagger api description
    feat(Docker): add Dockerfile and docker-compose

--- src/main/java/by/vadarod/bankapi/BankApiApplication.java
+++ src/main/java/by/vadarod/bankapi/BankApiApplication.java
@@ -1,15 +1,18 @@
 package by.vadarod.bankapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@Slf4j
 public class BankApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BankApiApplication.class, args);
+        log.info("Access to SWAGGER UI:{}", "http://localhost:24110/swagger-ui/index.html");
     }
 
 }
--- src/main/java/by/vadarod/bankapi/controllers/BankApiController.java
+++ src/main/java/by/vadarod/bankapi/controllers/BankApiController.java
@@ -2,6 +2,10 @@ package by.vadarod.bankapi.controllers;
 
 import by.vadarod.bankapi.dto.response.RateResponse;
 import by.vadarod.bankapi.services.ExchangeRateService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -17,15 +21,28 @@ import java.time.LocalDate;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Tag(name="Bank API controller", description="Provides methods to load exchange rates and retrieve rate by code(current_id) and date")
 public class BankApiController {
     private final ExchangeRateService exchangeRateService;
 
+
     @GetMapping("/load")
+    @Operation(summary = "Load exchange rates by date", description = "Returns number of rates read")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully loaded"),
+            @ApiResponse(responseCode = "400", description = "Error: date is greater than current or wrong given date format")
+    })
     public ResponseEntity<String> loadExchangeRate(@RequestParam(name = "date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
         return ResponseEntity.status(HttpStatus.OK).body(String.format("Read rate list size=%d", exchangeRateService.loadExchangeRateByDate(date)));
     }
 
     @GetMapping("/rates/{currentId}")
+    @Operation(summary = "Get exchange rate by code(current_id) and date", description = "Returns rate entity")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully read"),
+            @ApiResponse(responseCode = "400", description = "Error: date is greater than current or wrong given date format"),
+            @ApiResponse(responseCode = "404", description = "Error: rate entity was not found(not loaded before)")
+    })
     public ResponseEntity<RateResponse> getExchangeRate(@PathVariable(name = "currentId") Long currentId, @RequestParam(name = "date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
         return ResponseEntity.status(HttpStatus.OK).body(exchangeRateService.getExchangeRateByCurrentIdAndDate(currentId, date));
     }
